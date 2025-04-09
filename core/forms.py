8f7bf5a9a952ed759cb506f2c5d743f24a2bfdb6
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-04-09 23:56:46 +0530

    fixing ui issue

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Question, Answer, User
+from django.core.exceptions import ValidationError
+
 
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email address is already registered.")
+        return email
+
+
 class LoginForm(AuthenticationForm):
     pass
+
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -20,6 +30,7 @@ class QuestionForm(forms.ModelForm):
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3}),
         }
+
 
 class AnswerForm(forms.ModelForm):
     class Meta:
